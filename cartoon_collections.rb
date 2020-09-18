46f3a4b1a5eb6d255503e8122f6e6dfbc251a533
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 02:47:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,13 @@ def summon_captain_planet(calls)
   calls.map {|chant| chant.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long)
+  long.map do |word|
+    if word.length > 4
+      true
+    else
+      false
+    end
 end
 
 def find_the_cheese# code an argument here
