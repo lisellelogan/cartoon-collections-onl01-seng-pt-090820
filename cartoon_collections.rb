9f410833dd312707f5792a9be8cf9f8a73bc24d9
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 02:35:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  # Your code here
+  calls.map {|chant| chant.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
