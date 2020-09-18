e2d9e3cc8dae76da16a6497c8b53fc917613a41b
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 03:31:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ def long_planeteer_calls(long)
 end
 
 def find_the_cheese(cheese)
-  cheese.detect {|type| type.to_a.include?("cheddar", "gouda", "camenbert")}
+  cheese.detect {|type| type.include?("cheddar", "gouda", "camenbert")}
 end
